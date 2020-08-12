c00b741479692abc8e5b0e6ef82d1362d5535081
Johnson Mohamed <mohamedjohnson31@gmail.com>
2020-08-12 20:18:24 +0000

    :seedling: Navigation Stack Implemented

---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default HomeScreen = ({ navigation }) => {
+    return (
+        <>
+            <Text>Welcome </Text>
+        </>
+    )
+}
